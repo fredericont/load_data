42863bccf4a0733e9746cf8444085f2402808cc5
João Guedes <115195936+guedes-jr@users.noreply.github.com>
2024-04-03 22:55:59 -0300

    Update urls.py
    ajuste da autenticação

--- projeto_upload_arquivos/projeto_upload_arquivos/urls.py
+++ projeto_upload_arquivos/projeto_upload_arquivos/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from app_upload_arquivos import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('cadastro/', views.cadastro, name='cadastro')
+    path('', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('anexo/', views.anexo, name='anexo')
 ]
